63aa21fe4f272f61f0b1cf6df554c1d0e7bdf666
ChrisKnowsDev <chrisharley81@gmail.com>
2021-05-10 14:26:01 -0400

    displaying tags and tag count in index page

--- src/components/tags.js
+++ src/components/tags.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import setupTags from '../assets/utils/setup-tags'
+import { Link } from 'gatsby'
+import slugify from 'slugify'
 
 const Tags = ({ reviews }) => {
   const newTags = setupTags(reviews)
 
   return (
     <div>
-      <h3>tags component</h3>
+      <h4>Search Tags</h4>
+      <div className="tags-list">
+        {newTags.map((tag, index) => {
+          const [text, value] = tag
+          const slug = slugify(text, { lower: true })
+
+          return (
+            <Link to={`/tags/${slug}`} key={index}>
+              {text} ({value})
+            </Link>
+          )
+        })}
+      </div>
     </div>
   )
 }
