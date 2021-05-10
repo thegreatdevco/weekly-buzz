591668bd522f84066e5340b9801e5b95ae1a238b
ChrisKnowsDev <chrisharley81@gmail.com>
2021-05-10 15:50:48 -0400

    footer and styled for mobile

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { css } from '@emotion/react'
 
 const Footer = () => {
   return (
-    <div>
-      <h3>footer component</h3>
+    <div
+      css={css`
+        background: var(--color-dark);
+        color: var(--color-light);
+        text-align: center;
+        padding: 1rem;
+        line-height: 2;
+
+        a {
+          color: var(--color-primary);
+        }
+      `}
+    >
+      <p>&copy; Weekly Buzz {new Date().getFullYear()}</p>
+      <p>
+        Designed & Developed By: <a href="thegreatdevco.com">TGDC</a>
+      </p>
     </div>
   )
 }
