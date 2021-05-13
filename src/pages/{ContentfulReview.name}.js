3e42edf2f864c7af9610ffc85172161c6b28ad03
ChrisKnowsDev <chrisharley81@gmail.com>
2021-05-13 10:17:54 -0400

    header image and content

--- src/pages/{ContentfulReview.name}.js
+++ src/pages/{ContentfulReview.name}.js
@@ -3,22 +3,80 @@ import { Link, graphql } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout'
 import slugify from 'slugify'
+import { css } from '@emotion/react'
 
 const ReviewTemplate = ({ data }) => {
-  return <Layout>{data.contentfulReview.name}</Layout>
+  const {
+    name,
+    company,
+    rank,
+    category,
+    image,
+    description: { description },
+  } = data.contentfulReview
+
+  const pathToImage = getImage(image)
+
+  return (
+    <Layout>
+      {/* header */}
+      <header
+        css={css`
+          height: 40vh;
+          position: relative;
+          margin-bottom: 2rem;
+
+          .header-img {
+            height: 100%;
+            border-radius: var(--border-radius);
+          }
+
+          .header-content {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            color: var(--color-light);
+            background: rgba(0, 0, 0, 0.4);
+          }
+
+          p {
+            color: var(--color-primary);
+            margin-bottom: 1rem;
+          }
+        `}
+      >
+        <GatsbyImage image={pathToImage} alt={name} className="header-img" />
+        <div className="header-content">
+          <p>{company}</p>
+          <h1>{name}</h1>
+        </div>
+      </header>
+      {/* info */}
+      {/* description */}
+      {/* featured reviews */}
+    </Layout>
+  )
 }
 
 export const query = graphql`
   query getSingleReview($name: String) {
     contentfulReview(name: { eq: $name }) {
       name
+      category
       company
+      rank
       image {
-        gatsbyImageData(placeholder: TRACED_SVG, layout: CONSTRAINED)
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
+      description {
         description
       }
-      rank
-      category
     }
   }
 `
