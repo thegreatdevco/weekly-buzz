d1241f73fb96f17b3e40eeaea6373af5f800e229
ChrisKnowsDev <chrisharley81@gmail.com>
2021-05-13 14:27:23 -0400

    programmatic tag search functionality working

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import Reviews from '../components/reviews'
 
-const TagTemplate = () => {
+const TagTemplate = ({ data }) => {
+  const reviews = data.allContentfulReview.nodes
+
   return (
-    <div>
-      <h1>tag template page</h1>
-    </div>
+    <Layout>
+      <div className="tags-page">
+        <h3>All reviews containing the </h3>
+        <Reviews reviews={reviews} />
+      </div>
+    </Layout>
   )
 }
+
+export const query = graphql`
+  query getTaggedReviews($tag: String) {
+    allContentfulReview(
+      sort: { fields: name, order: ASC }
+      filter: { content: { tags: { eq: $tag } } }
+    ) {
+      nodes {
+        id
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+        company
+        category
+        name
+      }
+    }
+  }
+`
 
 export default TagTemplate
